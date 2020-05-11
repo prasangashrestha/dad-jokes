73a853ef9174a89d632458463bd72cba0427c6a4
Prasanga <101890972@student.swin.edu.au>
2020-05-12 05:03:27 +1000

    Added styling on JokeList container

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
-
+import './App.css'
 import JokeList from './JokeList';
+
 
 export default class App extends Component {
   
   render() {
     return (
-      <div>
-        <h1>Dad Jokes</h1>
+      <div className='App'>
+        
         <JokeList />
       </div>
     )
--- src/JokeList.js
+++ src/JokeList.js
@@ -1,17 +1,47 @@
 import React, { Component } from 'react'
 import axios from 'axios';
+import './JokeList.css'
 
 export default class JokeList extends Component {
+    static defaultProps = {
+        numJokesToGet: 10
+    }
+
+    state = {
+        jokes: []
+    }
+
     async componentDidMount() {
-        let res = await axios.get('https://icanhazdadjoke.com/',
+        let jokes = []
+        while(jokes.length < this.props.numJokesToGet){
+            let res = await axios.get('https://icanhazdadjoke.com/',
                    {headers: {Accept: 'application/json'}});
-        console.log(res.data.joke)
+            jokes.push(res.data.joke);
+        }
+
+        this.setState({jokes: jokes})
   
     }
     render() {
+        const jokes = this.state.jokes.map(joke => (
+            <p>{joke}</p>
+        ))
+
         return (
-            <div>
-                
+
+            <div className='JokeList'>
+                <div className="JokeList-sidebar">
+                    <h1 className="JokeList-title">
+                        <span>Dad</span> Jokes
+                        <img src="https://assets.dryicons.com/uploads/icon/svg/8927/0eb14c71-38f2-433a-bfc8-23d9c99b3647.svg" alt='ss'/>
+                    </h1>
+                    <button className="JokeList-getmore">Get Jokes</button>
+                </div>
+                <div className="JokeList-jokes">
+                     {jokes}
+                </div>
+                   
+            
             </div>
         )
     }
